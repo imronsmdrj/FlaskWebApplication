1b07e0a4f542d36ac0bd38e082ad7d2f8a139365
imronsmdrj <imronsumadireza@gmail.com>
2021-10-08 10:51:53 +0700

    upload all file

---
+++ website/__init__.py
@@ -0,0 +1,47 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = "helloworld"
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix="/") 
+    app.register_blueprint(auth, url_prefix="/")
+
+    from .models import User, Post, Comment, Like
+
+    create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = "auth.login"
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
+    #url_prefix itu untuk menambahkan kata di depan slash secara default, kalau gamau yaudah di kosongin kaya sekarang
+
+#    @app.route("/")
+#    def home():
+#        return "<h1>Hello Imron Sumadireja</h1>"
+
+#    @app.route("/profile")
+#   def profile():
+#        return "<h1>Profile Imron Sumadireja</h1>"
+    return app
+
+def create_database(app):
+    if not path.exists("website/" + DB_NAME):
+        db.create_all(app=app)
+        print("Created Database!")
---
+++ website/models.py
@@ -0,0 +1,37 @@
+from enum import unique
+
+from sqlalchemy.orm import backref
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    username = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now()) #default=func untuk menyesuaikan jam di mana kamu berada
+    posts = db.relationship('Post', backref='user', passive_deletes=True) #cari tau lagi mengenai backref
+    comment = db.relationship('Comment', backref='user', passive_deletes=True)
+    likes = db.relationship('Like', backref="user", passive_deletes=True)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.Text, nullable=False)
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable=False) #ondelete maksudnya, ketika id user di delete maka semua post yang ditulis dia akan ter delete juga
+    comment = db.relationship('Comment', backref="post", passive_deletes=True)
+    likes = db.relationship('Like', backref="post", passive_deletes=True)
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id', ondelete="CASCADE"), nullable=False)
+
+class Like(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id', ondelete="CASCADE"), nullable=False)
